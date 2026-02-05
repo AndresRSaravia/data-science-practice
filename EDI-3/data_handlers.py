fdbbc8d8ced38ba28206c8da493bd12e84f60ca9
A. R. Saravia <andresromansaravia@gmail.com>
2026-02-05 09:19:10 -0300

    EDI-3 update

--- EDI-3/data_handlers.py
+++ EDI-3/data_handlers.py
@@ -1,5 +1,7 @@
 import json
 from constant_values import test1_values,test2_values
+import pandas as pd
+import numpy as np
 
 def retrieve(filename):
 	with open(filename, 'r') as file:
@@ -18,7 +20,7 @@ def get_values(raw_row_test,test_values,offset=0):
 	for i in range(n):
 		answer = raw_row_test[i]
 		values = test_values[i+1]
-		print(answer,values)
+		#print(answer,values)
 		if answer is not None and values is not None:
 			row_test.append((answer,values[answer-offset]))
 		elif answer is not None and values is None:
@@ -26,7 +28,6 @@ def get_values(raw_row_test,test_values,offset=0):
 		else:
 			row_test.append((None,None))
 	return row_test
-	
 
 def process_row(key,raw_row):
 	row_basic = [key,raw_row["año/división"],raw_row["peso"],raw_row["altura"]]
@@ -38,6 +39,37 @@ def process_row(key,raw_row):
 		row_comment = raw_row["comentario"]
 	else:
 		row_comment = None
-	print(row_test1,row_test2)
+	#print(row_test1,row_test2)
 	row = row_basic + row_test1 + row_test2 + [row_comment]
 	return row
+
+def process_group(df,test_group):
+	gcolumns = list(map(lambda x: f"t2r{x:02}", test_group))
+	return sum(map(lambda x: df[x].apply(lambda x: x[1]), gcolumns))
+
+def process_incons(df,pairs):
+	zero_data = np.zeros((df.shape[0],1))
+	df_incons = pd.DataFrame(zero_data, columns = ["Inconsistencia"])
+	for (i,j) in pairs:
+		dfi = df[f"t2r{i:02}"].apply(lambda x: x[1])
+		dfj = df[f"t2r{j:02}"].apply(lambda x: x[1])
+		#print(i,j)
+		#print("i",dfi)
+		#print("j",dfj)
+		df_incons["Inconsistencia"] += (dfi-dfj).abs()
+	typical = lambda x: ((x,"Típica") if x<19 else ((x,"Atípica") if x<23 else (x,"Muy atípica")))
+	df_incons["Inconsistencia"] = df_incons["Inconsistencia"].apply(typical)
+	return df_incons
+
+def process_fours(df,answers,column_name):
+	zero_data = np.zeros((df.shape[0],1))
+	df_fours = pd.DataFrame(zero_data, columns = [column_name])
+	print(df_fours)
+	for i in answers:
+		dff = df[f"t2r{i:02}"].apply(lambda x: 1 if x[1] == 4 else 0)
+		print("sasssssssssssssssssss", df.shape, dff.shape, df_fours.shape)
+		print("(1)", df[f"t2r{i:02}"])
+		print("(2)", dff)
+		print("(3)", dff+df_fours)
+		df_fours += dff
+	return df_fours
--- EDI-3/processor.py
+++ EDI-3/processor.py
@@ -1,11 +1,19 @@
-from data_handlers import retrieve,get_characters,process_row
-from constant_values import test2_values,test2_groups
+from data_handlers import retrieve,process_row,process_group,process_incons,process_fours
+from constant_values import test2_groups,incons_pairs,critical_fours
 import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 
 def main():
-	raw_data = retrieve('raw_data.json')
+	raw_data = {
+		"666":{
+			"año/división":"8Z",
+			"test1":"|S|5-|34121555-|523135|55543|1454|-|322111-|",
+			"peso":"-",
+			"altura":"-",
+			"test2":"|420110302013205331|220350150155010010|500105401010150200|105310013020015100|5022000500000020504|"
+		}
+	}
 	#retrieve('raw_data.json')
 	columns_basic = ["n°", "año/división", "peso", "altura"]
 	columns_test1 = list(map(lambda x: f"t1r{x:02}", list(range(1,36))))
@@ -21,13 +29,18 @@ def main():
 	print(df_data)
 	group_data = {}
 	for keyg in test2_groups.keys():
-		gcolumns = list(map(lambda x: f"t2r{x:02}", test2_groups[keyg]))
-		print(test2_groups[keyg])
-		group_data[keyg] = sum(map(lambda x: df_data[x].apply(lambda x: x[1]), gcolumns))
+		group_data[keyg] = process_group(df_data,test2_groups[keyg])
 	df_group = pd.concat(group_data.values(), axis=1)
 	df_group.columns = group_data.keys()
-	df_data = pd.concat([df_data, df_group], axis=1)
+	df_incons = process_incons(df_data,incons_pairs)
+	print("aaa")
+	df_crit_fours = process_fours(df_data,critical_fours,"IF")
+	df_fours = process_fours(df_data,list(range(1,92)),"NI")
+	df_data_list = [df_data, df_group, df_incons, df_crit_fours, df_fours]
+	df_data = pd.concat(df_data_list, axis=1)
 	print(df_data)
+	print(df_fours)
+	print(df_crit_fours)
 	return df_data
 
 df_data = main()
