da9ff771d8453e404a64e5200feb8d1138a2d72d
A. R. Saravia <andresromansaravia@gmail.com>
2026-02-07 02:41:00 -0300

    major update

--- EDI-3/data_handlers.py
+++ EDI-3/data_handlers.py
@@ -1,8 +1,7 @@
 import json
-from constant_values import test1_values,test2_values
 
-def retrieve(filename):
-	with open(filename, 'r') as file:
+def retrieve_json(filename):
+	with open("json/"+filename+".json", "r") as file:
 			data = json.load(file)
 	return data
 
@@ -18,13 +17,13 @@ def get_values(raw_row_test,test_values,offset=0,extrachars=[""]):
 	row_test = []
 	for i in range(n):
 		answer = raw_row_test[i]
-		values = test_values[i+1]
-		#print(answer,values)
+		values = test_values[str(i+1)]
+		#print(i,answer,values)
 		if answer in extrachars:
 			row_test.append(answer)
-		elif answer is not None and values is not None:
+		elif (answer is not None) and (values is not None):
 			row_test.append((answer,values[answer-offset]))
-		elif answer is not None and values is None:
+		elif (answer is not None) and (values is None):
 			row_test.append((answer,None))
 		else:
 			row_test.append((None,None))
@@ -34,6 +33,11 @@ def process_row(key,raw_row):
 	row_basic = [key,raw_row["año/división"],raw_row["peso"],raw_row["altura"]]
 	raw_row_test1 = get_characters(raw_row["test1"],"|","-",extrachars=["S","N"])
 	raw_row_test2 = get_characters(raw_row["test2"],"|","-")
+	test1_values = retrieve_json("test1_values")
+	test2_values = retrieve_json("test2_values")
+	is_no_value = lambda x: x if x != "no value" else None
+	test1_values = {x:is_no_value(test1_values[x]) for x in test1_values.keys()}
+	test2_values = {x:is_no_value(test2_values[x]) for x in test2_values.keys()}
 	row_test1 = get_values(raw_row_test1,test1_values,1,["S","N"])
 	row_test2 = get_values(raw_row_test2,test2_values)
 	if "comentario" in raw_row.keys():
@@ -43,10 +47,36 @@ def process_row(key,raw_row):
 	row = row_basic + row_test1 + row_test2 + [row_comment]
 	return row
 
-def process_group(df,keyg,test_group):
-	gcolumns = list(map(lambda x: f"t2r{x:02}", test_group))
+def process_nans(df,columns,tag):
+	df_nans = 0
+	bool_to_int = lambda x: int(x)
+	ncolumns = list(map(lambda x: f"{tag}{x:02}", columns))
+	for keyn in ncolumns:
+		df_nans += df[keyn].apply(lambda x: x[0]).isna().apply(bool_to_int)
+	tuple_form = lambda x: (x,len(columns),round(x/len(columns),3))
+	df_nans = df_nans.apply(tuple_form)
+	return df_nans
+
+def process_proportions(df,keyg,group):
+	gcolumns = list(map(lambda x: f"t1r{x:02}", group))
+	#print(df[gcolumns])
 	df_keyg = sum(map(lambda x: df[x].apply(lambda x: x[1]), gcolumns))
+	tuple_form = lambda x: (x,len(gcolumns),round(x/len(gcolumns),3))
+	df_keyg = df_keyg.apply(tuple_form)
 	return df_keyg
+
+def process_group(df,keyg,group,T_score):
+	gcolumns = list(map(lambda x: f"t2r{x:02}", group))
+	if_none_zero = lambda x: x if x is not None else 0
+	df_keyg = sum(map(lambda x: df[x].apply(lambda x: if_none_zero(x[1])), gcolumns))
+	df_keyg = df_keyg.apply(lambda x: (x,T_score[str(int(x))]))
+	return df_keyg
+
+def process_meta_group(df,keymg,meta_group): #,T_score
+	mgcolumns = list(map(lambda x: f"{x}", meta_group))
+	df_keymg = sum(map(lambda x: df[x].apply(lambda x: x[1]), mgcolumns))
+	df_keymg = df_keymg.apply(lambda x: (x)) #,T_score[str(int(x))]
+	return df_keymg
 
 def typical(x,limit1,limit2):
 	if x<limit1:
@@ -80,9 +110,3 @@ def process_fours(df,answers,limits):
 		df_fours += dff
 	df_fours = df_fours.apply(lambda x: typical(x,limit1,limit2))
 	return df_fours
-
-def process_nans(df,columns,tag):
-	df_nans = 0
-	for column in columns:
-		df_nans += df[f"{tag}{column:02}"].apply(lambda x: x[0]).isna().apply(lambda x: 1 if x else 0)
-	return df_nans
--- EDI-3/processor.py
+++ EDI-3/processor.py
@@ -1,30 +1,67 @@
-from data_handlers import retrieve,process_row,process_group,process_incons,process_fours,process_nans
-from constant_values import test2_groups,incons_pairs,critical_fours
+from data_handlers import (
+	retrieve_json,
+	process_row,
+	process_incons,
+	process_fours,
+	process_nans,
+	process_group,
+	process_meta_group,
+	process_proportions
+	)
 import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.max_rows", None)
 
 def main():
-	raw_data = retrieve('raw_data.json')
+	print("retrieving raw data")
+	raw_data = retrieve_json("raw_data")
+	print("processing raw data into ")
 	columns_basic = ["n°", "año/división", "peso", "altura"]
 	columns_test1 = list(map(lambda x: f"t1r{x:02}", list(range(1,36))))
 	columns_test2 = list(map(lambda x: f"t2r{x:02}", list(range(1,92))))
 	columns_test = columns_test1 + columns_test2
 	comment = ["comentarios"]
-	col_names = columns_basic + columns_test + comment
-	df_data = pd.DataFrame(columns = col_names)
+	columns_initial = columns_basic + columns_test + comment
+	df_data = pd.DataFrame(columns = columns_initial)
 	for key in raw_data.keys():
-		print(key)
+		print(f"processing row {key}")
 		row = process_row(key,raw_data[key])
 		df_data.loc[len(df_data)] = row
-	for keyg in test2_groups.keys():
-		df_data[keyg] = process_group(df_data,keyg,test2_groups[keyg])
-	df_data["Inconsistencia"] = process_incons(df_data,incons_pairs)
+	print("test 1: processing mean for each section")
+	test1_groups = retrieve_json("test1_groups")
+	for keyg in test1_groups.keys():
+		group = test1_groups[keyg]
+		df_data[keyg] = process_proportions(df_data,keyg,group)
+		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t1r")
+	print("test 1: processing number of NaNs")
+	df_data["test1 (NaN)"] = process_nans(df_data,list(range(1,36)),"t1r")
+	print("test 2: processing IN")
+	incons_pairs = retrieve_json("test2_inconsistency_pairs")["0"]
+	df_data["IN"] = process_incons(df_data,incons_pairs)
+	print("test 2: processing IF")
+	critical_fours = retrieve_json("test2_critical_fours")["0"]
 	df_data["IF"] = process_fours(df_data,critical_fours,[3,5])
+	print("test 2: processing NI")
 	df_data["NI"] = process_fours(df_data,list(range(1,92)),[41,52])
-	df_data["test1 #nulos"] = process_nans(df_data,list(range(2,36)),"t1r")
-	df_data["test2 #nulos"] = process_nans(df_data,list(range(1,92)),"t2r")
-	print(df_data)
+	print("test 2: processing 12 group metrics")
+	groups = retrieve_json("test2_groups")
+	groups_T = retrieve_json("test2_groups_T_scores")
+	for keyg in groups.keys():
+		group = groups[keyg]
+		T_score = groups_T[keyg]
+		df_data[keyg] = process_group(df_data,keyg,group,T_score)
+		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t2r")
+	print("test 2: processing number of NaNs")
+	df_data["test2 (NaN)"] = process_nans(df_data,list(range(1,92)),"t2r")
+	print("test 2: processing meta group metrics")
+	meta_groups = retrieve_json("test2_meta_groups")
+	#meta_groups_T = retrieve_json("test2_meta_groups_T_scores")
+	for keymg in meta_groups.keys():
+		meta_group = meta_groups[keymg]
+		#T_score = meta_groups_T[keymg]
+		df_data[keymg] = process_meta_group(df_data,keymg,meta_group) #,T_score
+	#T12_scores = retrieve_json("T12_scores")
+	#print(df_data)
 	return df_data
 
 df_data = main()
