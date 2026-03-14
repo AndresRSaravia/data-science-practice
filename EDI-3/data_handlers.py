3102cb0bc49c7c1dd73eaea6ea0ab425d719e664
A. R. Saravia <andresromansaravia@gmail.com>
2026-03-14 17:04:47 -0300

    major processor update

--- EDI-3/data_handlers.py
+++ EDI-3/data_handlers.py
@@ -10,6 +10,8 @@ def get_characters(string,delchar,nonechar,extrachars=[""]):
 	str_list = list(str_del)
 	clean = lambda x: None if x==nonechar else (x if x in extrachars else int(x))
 	result = [clean(x) for x in str_list]
+	if extrachars!=[""]:
+		result[0] = "X"
 	return result
 
 def get_values(raw_row_test,test_values,offset=0,extrachars=[""]):
@@ -22,11 +24,11 @@ def get_values(raw_row_test,test_values,offset=0,extrachars=[""]):
 		if answer in extrachars:
 			row_test.append(answer)
 		elif (answer is not None) and (values is not None):
-			row_test.append((answer,values[answer-offset]))
+			row_test.extend([answer,values[answer-offset]])
 		elif (answer is not None) and (values is None):
-			row_test.append((answer,None))
+			row_test.extend([answer,None])
 		else:
-			row_test.append((None,None))
+			row_test.extend([None,None])
 	return row_test
 
 def process_row(key,raw_row):
@@ -44,7 +46,7 @@ def process_row(key,raw_row):
 	is_no_value = lambda x: x if x != "no value" else None
 	test1_values = {x:is_no_value(test1_values[x]) for x in test1_values.keys()}
 	test2_values = {x:is_no_value(test2_values[x]) for x in test2_values.keys()}
-	row_test1 = get_values(raw_row_test1,test1_values,1,["S","N"])
+	row_test1 = get_values(raw_row_test1,test1_values,1,["S","N","X"])
 	row_test2 = get_values(raw_row_test2,test2_values)
 	if "comentario" in raw_row.keys():
 		row_comment = raw_row["comentario"]
@@ -58,34 +60,29 @@ def process_nans(df,columns,tag):
 	bool_to_int = lambda x: int(x)
 	ncolumns = list(map(lambda x: f"{tag}{x:02}", columns))
 	for keyn in ncolumns:
-		df_nans += df[keyn].apply(lambda x: x[0]).isna().apply(bool_to_int)
-	tuple_form = lambda x: (x,len(columns),round(x/len(columns),3))
-	df_nans = df_nans.apply(tuple_form)
+		df_nans += df[keyn].isna().apply(bool_to_int)
 	return df_nans
 
-def process_proportions(df,keyg,group):
-	gcolumns = list(map(lambda x: f"t1r{x:02}", group))
-	#print(df[gcolumns])
-	df_keyg = sum(map(lambda x: df[x].apply(lambda x: x[1]), gcolumns))
-	tuple_form = lambda x: (x,len(gcolumns),round(x/len(gcolumns),3))
-	df_keyg = df_keyg.apply(tuple_form)
+def process_sum(df,keyg,group):
+	gcolumns = list(map(lambda x: f"t1v{x:02}", group))
+	df_keyg = sum(map(lambda x: df[x], gcolumns))
 	return df_keyg
 
 def process_group(df,keyg,group,T_score):
-	gcolumns = list(map(lambda x: f"t2r{x:02}", group))
+	gcolumns = list(map(lambda x: f"t2v{x:02}", group))
 	if_none_zero = lambda x: x if x is not None else 0
-	df_keyg = sum(map(lambda x: df[x].apply(lambda x: if_none_zero(x[1])), gcolumns))
-	df_keyg = df_keyg.apply(lambda x: (x,T_score[str(int(x))]))
-	return df_keyg
+	df_keyg = sum(map(lambda x: df[x].apply(lambda x: if_none_zero(x)), gcolumns))
+	df_keyg_T = df_keyg.apply(lambda x: T_score[str(int(x))])
+	return df_keyg,df_keyg_T
 
 def process_meta_group(df,keymg,meta_group,limits,T_score):
 	mgcolumns = list(map(lambda x: f"{x}", meta_group))
-	df_keymg = sum(map(lambda x: df[x].apply(lambda x: x[1]), mgcolumns))
+	df_keymg = sum(map(lambda x: df[x+" (PT)"], mgcolumns))
 	l1,l2 = limits
 	lim_fun = lambda x: l1 if x<=l1 else (l2 if x>=l2 else x)
 	lim_fun_s = lambda x: str(int(lim_fun(x)))
-	df_keymg = df_keymg.apply(lambda x: (x,T_score[lim_fun_s(x)]))
-	return df_keymg
+	df_keymg_T = df_keymg.apply(lambda x: T_score[lim_fun_s(x)])
+	return df_keymg,df_keymg_T
 
 def typical(x,limit1,limit2):
 	if x<limit1:
@@ -99,23 +96,17 @@ def typical(x,limit1,limit2):
 def process_incons(df,pairs):
 	df_incons = 0
 	for (i,j) in pairs:
-		dfi = df[f"t2r{i:02}"].apply(lambda x: x[1])
-		dfj = df[f"t2r{j:02}"].apply(lambda x: x[1])
-		#print(i,j)
-		#print("i",dfi)
-		#print("j",dfj)
+		dfi = df[f"t2v{i:02}"]
+		dfj = df[f"t2v{j:02}"]
 		df_incons += (dfi-dfj).abs()
-	df_incons = df_incons.apply(lambda x: typical(x,19,23))
+	#df_incons = df_incons.apply(lambda x: typical(x,19,23))
 	return df_incons
 
 def process_fours(df,answers,limits):
 	limit1,limit2 = limits
 	df_fours = 0
 	for answer in answers:
-		dff = df[f"t2r{answer:02}"].apply(lambda x: 1 if x[1] == 4 else 0)
-		#print("(1)", df[f"t2r{answer:02}"])
-		#print("(2)", dff)
-		#print("(3)", dff+df_fours)
+		dff = df[f"t2v{answer:02}"].apply(lambda x: 1 if x == 4 else 0)
 		df_fours += dff
-	df_fours = df_fours.apply(lambda x: typical(x,limit1,limit2))
+	#df_fours = df_fours.apply(lambda x: typical(x,limit1,limit2))
 	return df_fours
--- EDI-3/processor.py
+++ EDI-3/processor.py
@@ -6,7 +6,7 @@ from data_handlers import (
 	process_nans,
 	process_group,
 	process_meta_group,
-	process_proportions
+	process_sum
 	)
 import pandas as pd
 from warnings import simplefilter
@@ -17,26 +17,34 @@ pd.set_option("display.max_rows", None)
 def main():
 	print("retrieving raw data")
 	raw_data = retrieve_json("raw_data")
-	print("processing raw data into ")
-	columns_basic = ["colegio", "n°", "año/división", "peso", "altura"]
-	columns_test1 = list(map(lambda x: f"t1r{x:02}", list(range(1,36))))
-	columns_test2 = list(map(lambda x: f"t2r{x:02}", list(range(1,92))))
-	columns_test = columns_test1 + columns_test2
+	print("processing raw data into dataframe")
+	col_basic = ["n°", "colegio", "año/división", "peso", "altura"]
+	test1 = list(map(lambda x: [f"t1a{x:02}",f"t1v{x:02}"], list(range(2,36))))
+	col_test1 = ["t1a01"] + [item for sublist in test1 for item in sublist]
+	test2 = list(map(lambda x: [f"t2a{x:02}",f"t2v{x:02}"], list(range(1,92))))
+	col_test2 = [item for sublist in test2 for item in sublist]
+	col_test = col_test1 + col_test2
 	comment = ["comentarios"]
-	columns_initial = columns_basic + columns_test + comment
-	df_data = pd.DataFrame(columns = columns_initial)
+	col_initial = col_basic + col_test + comment
+	#print(col_initial)
+	df_data = pd.DataFrame(columns = col_initial)
 	for key in raw_data.keys():
-		print(f"processing row {key}")
+		#print(f"processing row {key}")
 		row = process_row(key,raw_data[key])
 		df_data.loc[len(df_data)] = row
-	print("test 1: processing mean for each section")
+	print("test 1: processing points for each section")
 	test1_groups = retrieve_json("test1_groups")
 	for keyg in test1_groups.keys():
 		group = test1_groups[keyg]
-		df_data[keyg] = process_proportions(df_data,keyg,group)
-		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t1r")
-	print("test 1: processing number of NaNs")
-	df_data["test1 (NaN)"] = process_nans(df_data,list(range(1,36)),"t1r")
+		df_data[keyg+f" (máx:{len(group)*3})"] = process_sum(df_data,keyg,group)
+		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t1a")
+	print("test 1: processing total points")
+	allgroup = []
+	for keyg in test1_groups.keys():
+		allgroup.extend(test1_groups[keyg])
+	df_data[f"test1 total (máx:{len(allgroup)*3})"] = process_sum(df_data,keyg,allgroup)
+	print("test 1: processing total number of NaNs")
+	df_data["test1 (NaN)"] = process_nans(df_data,list(range(1,36)),"t1a")
 	print("test 2: processing IN")
 	incons_pairs = retrieve_json("test2_inconsistency_pairs")["0"]
 	df_data["IN"] = process_incons(df_data,incons_pairs)
@@ -51,10 +59,12 @@ def main():
 	for keyg in groups.keys():
 		group = groups[keyg]
 		T_score = groups_T[keyg]
-		df_data[keyg] = process_group(df_data,keyg,group,T_score)
-		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t2r")
+		df_keyg,df_keyg_T = process_group(df_data,keyg,group,T_score)
+		df_data[keyg+" (PD)"] = df_keyg
+		df_data[keyg+" (PT)"] = df_keyg_T
+		df_data[keyg+" (Nan)"] = process_nans(df_data,group,"t2v")
 	print("test 2: processing number of NaNs")
-	df_data["test2 (NaN)"] = process_nans(df_data,list(range(1,92)),"t2r")
+	df_data["test2 (NaN)"] = process_nans(df_data,list(range(1,92)),"t2v")
 	print("test 2: processing meta group metrics")
 	meta_groups = retrieve_json("test2_meta_groups")
 	meta_groups_T = retrieve_json("test2_meta_groups_T_scores")
@@ -62,18 +72,19 @@ def main():
 		meta_group = meta_groups[keymg]
 		T_score = meta_groups_T[keymg]
 		limits = T_score["limits"]
-		#print(keymg,meta_group,T_score,limits)
-		df_data[keymg] = process_meta_group(df_data,keymg,meta_group,limits,T_score)
-	#T12_scores = retrieve_json("T12_scores")
-	#print(df_data)
+		df_keymg,df_keymg_T = process_meta_group(df_data,keymg,meta_group,limits,T_score)
+		df_data[keymg+" (SPT)"] = df_keymg
+		df_data[keymg+" (PT)"] = df_keymg_T
+	print("saving dataframe")
+	df_data.to_excel('xlsx/stats.xlsx')
 	return df_data
 
 df_data = main()
-df_data.to_excel('xlsx/stats.xlsx')
 
 """
 {
 	"666":{
+		"colegio":1,
 		"año/división":"8Z",
 		"test1":"|S|5-|34121555-|523135|55543|1454|-|322111-|",
 		"peso":"-",
